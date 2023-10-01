521f54bbde1a154089b4126282a37eea61451bd6
John Kennedy Aquino <qjkaaquino@tip.edu.ph>
2023-10-01 23:04:17 +0800

    Update App.js

--- HOA2_2/App.js
+++ HOA2_2/App.js
@@ -1,104 +1,142 @@
 import { useState } from 'react';
-import { Button, StyleSheet, Text, 
-  TextInput, View, FlatList, 
-  TouchableOpacity, ImageBackground, ScrollView} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ImageBackground,
+  TouchableOpacity,
+  FlatList,
+  ScrollView
+} from 'react-native';
 
 export default function App() {
-  const [enteredGoalText, setEnteredGoalText] = useState("")
-  const [courseGoals, setCourseGoals] = useState([])
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
 
-  function goalInputHandler(enteredText) {
+  function goalnputHandler(enteredText) {
     setEnteredGoalText(enteredText);
-  };
+  }
 
-{/*Bad Programming Practice*/}
-  function addGoalHandler(){
-    if (enteredGoalText.trim() === ''){
+  function addGoalHandler() {
+    if (enteredGoalText.trim() === '') {
       return;
     }
-    setCourseGoals((currentCourseGoals)=>
-    [...courseGoals, { id: Math.random().toString(), value: enteredGoalText}]);
+
+    setCourseGoals((currentCourseGoals) => [
+      ...courseGoals,
+      { id: Math.random().toString(), value: enteredGoalText },
+    ]);
     setEnteredGoalText('');
-  };
+  }
 
   return (
     <ImageBackground
-      source={require('./assets/bg.png')}
-      style={styles.backgroundImage}>
-
+      source={require('./assets/Abstract.jpg')}
+      style={styles.backgroundImage}
+    >
       <View style={styles.appContainer}>
+        <View style={styles.headerContainer}>
+          <Text style={styles.Title}>2023 Goals</Text>
+          <Text style={styles.subHeader}>Your Milestone Tracker</Text>
+        </View>
+
         <View style={styles.inputContainer}>
-          <TextInput style={styles.inputText} placeholder=' Your Course Goal' 
-          onChangeText={goalInputHandler} value={enteredGoalText}/>
-          <Button title='Add Goal' color='#CBBAB0'
-          onPress={addGoalHandler}/>
-        </View>
-
-        {/*<View style={styles.goalsContainer}> 
-          <Text>List of Goals</Text>
-      </View>*/}
-
-        <View style={styles.goalListContainer}>
-          <FlatList keyExtractor={(item, index) => item.id}
-            data={courseGoals} renderItem={itemData => (
-            <ScrollView style={styles.goalItem}>       
-              <Text style={styles.goalText}>{itemData.item.value}</Text>
-            </ScrollView>
-            )}
+          <TextInput
+            placeholder="Your Course Goal"
+            style={styles.inputText}
+            onChangeText={goalnputHandler}
+            value={enteredGoalText}
           />
+          <Button title="Add Goal" color="#5D4534" onPress={addGoalHandler} />
         </View>
+
+        <FlatList
+          keyExtractor={(item, index) => item.id}
+          data={courseGoals}
+          renderItem={(itemData) => (
+            <TouchableOpacity
+              onPress={() => {
+                setCourseGoals((prevGoals) =>
+                  prevGoals.filter((goal) => goal.id !== itemData.item.id)
+                );
+              }}
+            >
+              <View style={styles.goalItem}>
+                <Text style={styles.goalText}>{itemData.item.value}</Text>
+              </View>
+            </TouchableOpacity>
+          )}
+        />
       </View>
     </ImageBackground>
   );
 }
 
-const styles = StyleSheet.create({
-  backgroundImage: {
-    flex: 1,
-    resizeMode: 'stretch',
-  },
+const styles = {
   appContainer: {
     flex: 1,
-    paddingTop: 10,
-    paddingHorizontal: 15,
+    paddingTop:50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    marginBottom: 24,
-    borderBottomWidth: 5,
-    borderColor: '#9C949C',
+    marginBottom: 15,
+    borderBottomWidth: 1,                   //editt
+    borderBottomColor: '#282321',
   },
   inputText: {
     borderWidth: 2,
-    borderColor: '#333',
-    backgroundColor: '#CBBAB0',
-    padding: 10,
-    borderRadius: 20,
+    color: 'black',                         //edit
+    width: '70%',
     marginRight: 12,
+    padding: 10,
+    borderColor: '#61564A',
+    borderRadius: 8,
+    backgroundColor: '#B3AEA4',
     fontSize: 16,
+  },
+  goalContainer: {
+    flex: 5,
+    fontSize: 16,
+  },
+  backgroundImage: {
+    flex:1,
+    resizeMode: 'stretch',
+  },
+  headerContainer: {
+    backgroundColor: '#B3AEA4',
+    borderColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 60,
+    marginRight: 60,
+    marginTop: 1,
+    marginBottom: 1,
+    borderWidth: 2,
+    borderRadius: 20,
+  },
+  Title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#302821',
+    paddingBottom: 1,
+  },
+  subHeader: {
+    margin: 5,
+    fontSize: 15,
+    color: '#302821'
   },
   goalItem: {
     padding: 10,
-    backgroundColor: '#CBBAB0',
-    borderColor: '#383532',
-    borderWidth: 2,
-    borderRadius: 20,
+    backgroundColor: '#B3AEA4',
+    borderColor: '#333',
+    borderWidth: 1,
+    borderRadius: 8,
     marginBottom: 10,
-    marginRight: 100,
   },
-  goalText: {
-    fontSize: 12,
-    color: '#333',
-    fontSize: 16,
-  },
-  goalsContainer: {
-    fontSize: 16,
-    color: 'white',
-  },
-  goalListContainer: {
-    flex: 3,
-  }
-});
+};
